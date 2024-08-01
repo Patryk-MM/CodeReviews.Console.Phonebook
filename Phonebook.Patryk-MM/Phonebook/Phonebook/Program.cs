47f58c0319f984c248696d369a5a544f78084be0
Patryk-MM <patryk.mikitka@gmail.com>
2024-08-01 13:04:07 +0200

    Project setup.

---
+++ Phonebook.Patryk-MM/Phonebook/Phonebook/Program.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.Configuration;
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName)
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+string connectionString = configuration["ConnectionStrings:Phonebook"];
+Console.WriteLine(connectionString);
